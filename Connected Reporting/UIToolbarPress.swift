7074e363185c1edbb7f76939ac28b1e27e164bc1
thegithubgeek <wfrankw9@gmail.com>
2017-04-08 22:04:42 -0700

    Lots of good stuff XD

---
+++ Connected Reporting/UIToolbarPress.swift
@@ -0,0 +1,15 @@
+//
+//  UIToolbarPress.swift
+//  Connected Reporting
+//
+//  Created by Frank on 4/8/17.
+//  Copyright © 2017 Frank. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class UIToolbarPress:UIToolbar{
+    override func touchesBegan(){
+    
+    }
+}
